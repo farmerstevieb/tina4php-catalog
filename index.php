1ea241e819d21c5454e9aaaa7cd06e348e9c5853
Stephen Bacon <38105759+farmerstevieb@users.noreply.github.com>
2023-03-08 20:11:16 +0000

    rename to catalog
    from ecomm -> catalog

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use function Tina4\Initialize;
 require_once "./vendor/autoload.php";
 
 global $DBA;
-$DBA = new DataSQLite3("ecomm.db");
+$DBA = new DataSQLite3("catalog.db");
 
 $config = new Config(function($config) {
     (new Catalog())->addConfigMethods($config);
